7ba499c9633ebc0ace3cfd5b33d08a7f996a62a6
Michal Michalski <buyuk.dev@gmail.com>
2020-01-10 19:23:47 +0100

    Improved program.
    + performance measurement
    + option to run on CPU or with OpenCL on GPU

--- multiply.cl
+++ multiply.cl
@@ -1,22 +1,14 @@
-// Michal Michalski
-// -----------------------------------
-// Square Matrix Multiplication Kernel
-// A[N,N] * B[N,N] = C[N,N]
-// -----------------------------------
-
 __kernel void MultiplyMatrices(const int SIZE, const __global float* A, const __global float* B, __global float* C)
 {
-    // get identifiers of current work item which indicates data to be processed.
     const int row = get_global_id(0);
     const int col = get_global_id(1);
 
     // calculate dot product of A[row, *] and B[*, col]
     float sum = 0.0f;
-    for (int idx = 0; idx < K; idx ++) {
+    for (int idx = 0; idx < SIZE; idx ++) {
         sum += A[idx * SIZE + row] * B[col * SIZE + idx];
     }
 
     // save result
     C[col * SIZE + row] = sum;
 }
-
--- multiply.cpp
+++ multiply.cpp
@@ -2,142 +2,188 @@
 //
 // Square matrix multiplication SGEMM
 //
-// g++ -std=c++17 -o multiply -lOpenCL
+// Compilation on linux:
+//   g++ -std=c++17 -o multiply multiply.cpp -lOpenCL
+//
+// Compilation on macos:
+//   g++ -std=c++17 -o multiply multiply.cpp -framework OpenCL
 
+#define CL_SILENCE_DEPRECATION
 
-#include <cstdio>
+#include "utils.hpp"
+
 #include <iostream>
-#include <sstream>
-#include <fstream>
 #include <vector>
-#include <CL/cl.h>
+#include <chrono>
 
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif // __APPLE__
 
 using namespace std;
 
 
-const int MATRIX_SIZE = 3;
-const int WORK_GROUP_SIZE = 32;
+const int MATRIX_SIZE = 1024;
+const int WORK_GROUP_SIZE = 8;
 const char KERNEL_PATH[] = "multiply.cl";
 
 
-string load_kernel_source(const std::string& path) {
-    ifstream ifs(path);
-    if (!ifs) {
-        cerr << "file not found: " << path << endl;
-        return "";
+void multiplyOnCPU(const int SIZE, const vector<float>& A, const vector<float>& B, vector<float>& C) {
+    for (int row = 0; row < SIZE; ++row) {
+        for (int col = 0; col < SIZE; ++col) {
+            float sum = 0.0f;
+            for (int idx = 0; idx < SIZE; ++idx) {
+                sum += A[idx * SIZE + row] * B[col * SIZE + row];
+            }
+            C[row * SIZE + col] = sum;
+        }
     }
-    stringstream stream;
-    stream << ifs.rdbuf();
-    return stream.str();
 }
 
 
 int main(int argc, char* argv[]) {
-    string kernelSource = load_kernel_source(KERNEL_PATH);
+    auto args = parseArgs(argc, argv);
+
+    string kernelSource = loadKernelSource(KERNEL_PATH);
     if (kernelSource.size() == 0) {
         cerr << "Failed to load kernel from " << KERNEL_PATH << endl;
         return 1;
     }
-    cerr << "Loaded kernel source." << endl;
 
     // Initialize matrices with some test data.
-    vector<float> A(MATRIX_SIZE * MATRIX_SIZE);
-    vector<float> B(MATRIX_SIZE * MATRIX_SIZE);
-    vector<float> C(MATRIX_SIZE * MATRIX_SIZE);
+    const int kSquareSize = MATRIX_SIZE * MATRIX_SIZE;
+    vector<float> C(kSquareSize);
 
-    for (int i = 0; i < A.size(); i++) { A[i] = 1.0; }
+    vector<float> A(kSquareSize);
+    for (int i = 0; i < A.size(); i++) { A[i] = 3.0; }
+
+    vector<float> B(kSquareSize);
     for (int i = 0; i < B.size(); i++) { B[i] = 2.0; }
-    for (int i = 0; i < C.size(); i++) { C[i] = 0.0; }
 
     // Initialize OpenCL context.
-    cerr << "Initialize OpenCL" << endl;
     cl_platform_id platform = 0;
     cl_int ret = clGetPlatformIDs(1, &platform, nullptr);
-    cerr << "Number of available platforms: " << platform << endl;
+    cerr << "clGetPlatformIDs() --> " << getErrorString(ret) << endl;
 
     cl_device_id device = 0;
-    clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device, nullptr);
-    cerr << "Number of available devices: " << device << endl;
+    ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device, nullptr);
+    cerr << "clGetDeviceIDs() --> " << getErrorString(ret) << endl;
 
-    cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, nullptr);
-    cl_command_queue queue = clCreateCommandQueue(context, device, 0, nullptr);
+    cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &ret);
+    cerr << "clCreateContext() --> " << getErrorString(ret) << endl;
+
+    cl_command_queue queue = clCreateCommandQueue(context, device, 0, &ret);
+    cerr << "clCreateCommandQueue() --> " << getErrorString(ret) << endl;
 
     const int kDeviceNameMaxSize = 1024;
     char deviceName[kDeviceNameMaxSize];
-    clGetDeviceInfo(device, CL_DEVICE_NAME, kDeviceNameMaxSize, deviceName, nullptr);
-    cerr << "Device name is " << deviceName << endl;
+    ret = clGetDeviceInfo(device, CL_DEVICE_NAME, kDeviceNameMaxSize, deviceName, nullptr);
+    cerr << "clGetDeviceInfo() --> " << getErrorString(ret) << endl;
+    cerr << "Selected device is " << deviceName << endl;
 
     const char* kernelsList[] = { kernelSource.c_str() };
     const size_t kernelsSizeList[] = {kernelSource.size() };
     cl_program program = clCreateProgramWithSource(context, 1, kernelsList, kernelsSizeList, nullptr);
-    clBuildProgram(program, 1, nullptr, "", nullptr, nullptr);
-
-    cl_event event = nullptr;
-
-    // Print compile errors if occured.
-    size_t logSize = 0;
-    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &logSize);
-
-    char* messages = new char[1 + logSize];
-    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize, messages, nullptr);
-    messages[logSize] = '\0';
-
-    if (logSize > 10) {
-        cerr << "Compilation errors: " <<  messages << endl;
-        return 1;
-    }
-    delete messages;
+    ret = clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
+    cout << "clBuildProgram() --> " << getErrorString(ret) << endl;
 
     // Create buffers on device.
-    cerr << "Allocate buffers on device." << endl;
-    cl_mem bufA = clCreateBuffer(context, CL_MEM_READ_ONLY,  A.size() * sizeof(float), nullptr, nullptr);
-    cl_mem bufB = clCreateBuffer(context, CL_MEM_READ_ONLY,  B.size() * sizeof(float), nullptr, nullptr);
-    cl_mem bufC = clCreateBuffer(context, CL_MEM_READ_WRITE, C.size() * sizeof(float), nullptr, nullptr);
+    cl_mem bufA = clCreateBuffer(context, CL_MEM_READ_ONLY,  A.size() * sizeof(float), nullptr, &ret);
+    cout << "clCreateBuffer(A) --> " << getErrorString(ret) << endl;
+
+    cl_mem bufB = clCreateBuffer(context, CL_MEM_READ_ONLY,  B.size() * sizeof(float), nullptr, &ret);
+    cout << "clCreateBuffer(B) --> " << getErrorString(ret) << endl;
+
+    cl_mem bufC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, C.size() * sizeof(float), nullptr, &ret);
+    cout << "clCreateBuffer(C) --> " << getErrorString(ret) << endl;
 
     // Copy matrices to the device memory.
     cerr << "Copy input data to the device." << endl;
-    clEnqueueWriteBuffer(queue, bufA, CL_TRUE, 0, A.size() * sizeof(float), A.data(), 0, nullptr, nullptr);
-    clEnqueueWriteBuffer(queue, bufB, CL_TRUE, 0, B.size() * sizeof(float), B.data(), 0, nullptr, nullptr);
+    ret = clEnqueueWriteBuffer(queue, bufA, CL_TRUE, 0, A.size() * sizeof(float), A.data(), 0, nullptr, nullptr);
+    cerr << "clEnqueueWriteBuffer(A) --> " << getErrorString(ret) << endl;
+
+    ret = clEnqueueWriteBuffer(queue, bufB, CL_TRUE, 0, B.size() * sizeof(float), B.data(), 0, nullptr, nullptr);
+    cerr << "clEnqueueWriteBuffer(B) --> " << getErrorString(ret) << endl; 
 
     // Set kernel arguments.
-    cerr << "Configure kernel arguments." << endl;
-    cl_kernel kernel = clCreateKernel(program, "MultiplyMatrices", nullptr);
-    clSetKernelArg(kernel, 0, sizeof(int), static_cast<const void*>(&MATRIX_SIZE));
-    clSetKernelArg(kernel, 1, sizeof(cl_mem), static_cast<void*>(&bufA));
-    clSetKernelArg(kernel, 2, sizeof(cl_mem), static_cast<void*>(&bufB));
-    clSetKernelArg(kernel, 3, sizeof(cl_mem), static_cast<void*>(&bufC));
+    cl_kernel kernel = clCreateKernel(program, "MultiplyMatrices", &ret);
+    cerr << "clCreateKernel() --> " << getErrorString(ret) << endl;
+    
+    ret = clSetKernelArg(kernel, 0, sizeof(int), static_cast<const void*>(&MATRIX_SIZE));
+    cerr << "clSetKernelArg(0) --> " << getErrorString(ret) << endl;
+
+    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), static_cast<void*>(&bufA));
+    cerr << "clSetKernelArg(1) --> " << getErrorString(ret) << endl;
+
+    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), static_cast<void*>(&bufB));
+    cerr << "clSetKernelArg(2) --> " << getErrorString(ret) << endl;
+
+    ret = clSetKernelArg(kernel, 3, sizeof(cl_mem), static_cast<void*>(&bufC));
+    cerr << "clSetKernelArg(3) --> " << getErrorString(ret) << endl;
 
     // Add task to the queue.
-    cerr << "Execute task." << endl;
-    const size_t local[2] = { WORK_GROUP_SIZE, WORK_GROUP_SIZE };
-    const size_t global[2] = { MATRIX_SIZE, MATRIX_SIZE };
-    clEnqueueNDRangeKernel(queue, kernel, 2, nullptr, global, local, 0, nullptr, &event);
+    auto t1 = std::chrono::high_resolution_clock::now();
 
-    // Wait until completed and read results.
-    clWaitForEvents(1, &event);
-    clEnqueueReadBuffer(queue, bufC, CL_TRUE, 0, C.size() * sizeof(float), C.data(), 0, nullptr, nullptr);
+    if (args.find("--gpu") != args.end()) {
+        const size_t local[2] = { WORK_GROUP_SIZE, WORK_GROUP_SIZE };
+        const size_t global[2] = { MATRIX_SIZE, MATRIX_SIZE };
+        ret = clEnqueueNDRangeKernel(queue, kernel, 2, nullptr, global, local, 0, nullptr, nullptr);
+        cerr << "clEnqueueNDRangeKernel() --> " << getErrorString(ret) << endl;
+
+        // Read results.
+        ret = clEnqueueReadBuffer(queue, bufC, CL_TRUE, 0, C.size() * sizeof(float), C.data(), 0, nullptr, nullptr);
+        cerr << "clEnqueueReadBuffer() --> " << getErrorString(ret) << endl;
+
+        ret = clFlush(queue);
+        cerr << "clFlush() --> " << getErrorString(ret) << endl;
+
+        clFinish(queue);
+        cerr << "clFinish() --> " << getErrorString(ret) << endl;
+    }
+    else {
+        multiplyOnCPU(MATRIX_SIZE, A, B, C);
+    }
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+
+    cerr << "multiplication took " << duration << " us." << endl;
+
+    if (args.find("--print") != args.end()) {
+        cerr << "----------------------" << endl;
+        for (int i=0; i<10; ++i) {
+            for (int j=0; j<10; ++j) {
+                cout << C[i * MATRIX_SIZE + j] << " ";
+            }
+            cout << endl;
+        }
+        cerr << "----------------------" << endl;
+    }
 
     // Clean up OpenCL objects.
-    cerr << "Clean up." << endl;
-    clReleaseMemObject(bufA);
-    clReleaseMemObject(bufB);
-    clReleaseMemObject(bufC);
+    ret = clReleaseMemObject(bufA);
+    cerr << "clReleaseMemObject(A) --> " << getErrorString(ret) << endl;
 
-    clReleaseCommandQueue(queue);
-    clReleaseContext(context);
-    clReleaseProgram(program);
-    clReleaseKernel(kernel);
+    ret = clReleaseMemObject(bufB);
+    cerr << "clReleaseMemObject(B) --> " << getErrorString(ret) << endl;
 
-    cout << "RESULT" << endl;
-    for (int i=0; i<MATRIX_SIZE; ++i) {
-        for (int j=0; j<MATRIX_SIZE; ++j) {
-            cout << C[i * MATRIX_SIZE + j] << " ";
-        }
-        cout << endl;
-    }
+    ret = clReleaseMemObject(bufC);
+    cerr << "clReleaseMemObject(C) --> " << getErrorString(ret) << endl;
+
+    ret = clReleaseCommandQueue(queue);
+    cerr << "clReleaseCommandQueue() --> " << getErrorString(ret) << endl;
+
+    ret = clReleaseContext(context);
+    cerr << "clReleaseContext() --> " << getErrorString(ret) << endl;
+
+    ret = clReleaseProgram(program);
+    cerr << "clReleaseProgram() --> " << getErrorString(ret) << endl;
+
+    ret = clReleaseKernel(kernel);
+    cerr << "clReleaseKernel() --> " << getErrorString(ret) << endl;
 
     return 0;
 }
 
-// =================================================================================================
